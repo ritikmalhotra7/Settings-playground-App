d6df7cabf72a4cf7aea224e25be6aa188b44a9ad
Ritik Malhotra <malritik@gmail.com>
2024-10-02 23:18:30 +0530

    Changes 1. New Configuration changes handler architecture

--- app/src/main/java/com/example/settings_playground/model/ItemConfiguration.kt
+++ app/src/main/java/com/example/settings_playground/model/ItemConfiguration.kt
@@ -1,7 +1,37 @@
 package com.example.settings_playground.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class ItemConfiguration(
-    val id:String,
-    val string1:String,
-    val string2:String
-)
+    val id: String,
+    val string1: String,
+    var string2: String
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(string1)
+        parcel.writeString(string2)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ItemConfiguration> {
+        override fun createFromParcel(parcel: Parcel): ItemConfiguration {
+            return ItemConfiguration(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ItemConfiguration?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
